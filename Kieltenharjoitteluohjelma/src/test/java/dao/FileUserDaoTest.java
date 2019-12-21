d78572797bc2ce12ca8e8f4173a88e7e79c5bcb5
Jyrki <jyrki.kaukoranta@helsinki.fi>
2019-12-21 23:47:02 +0200

    screenshots added and minor fixes made

--- Kieltenharjoitteluohjelma/src/test/java/dao/FileUserDaoTest.java
+++ Kieltenharjoitteluohjelma/src/test/java/dao/FileUserDaoTest.java
@@ -1,8 +1,11 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import kieltenharjoitteluohjelma.dao.FileUserDao;
 import kieltenharjoitteluohjelma.domain.User;
+import org.junit.After;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.Before;
@@ -16,6 +19,14 @@ public class FileUserDaoTest {
     public void setUp() {
         this.connect = new TestConnection();
         userDao = new FileUserDao(connect);
+    }
+
+    @After
+    public void after() throws SQLException {
+        Connection connection = connect.connect();
+        PreparedStatement stmt = connection.prepareStatement("DELETE FROM Users WHERE Name='testi';");
+        stmt.executeUpdate();
+        connection.close();
     }
 
     @Test
@@ -43,6 +54,13 @@ public class FileUserDaoTest {
     @Test
     public void falseUserName() {
         assertFalse(userDao.checkPassword("h", "world"));
+    }
+
+    @Test
+    public void newUser() throws SQLException {
+        User user = new User("testi", "testi");
+        userDao.create(user);
+        assertEquals(user.getName(), "testi");
     }
 
 }
